58c446ad8057f699fec290654d499b0e5d6705f8
Badshah  Yadav <96272309+BadshahYadav@users.noreply.github.com>
2023-03-11 21:44:10 +0530

    Few changes made in Home page

---
+++ script.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal1");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnCloseModal2 = document.querySelector("button.close");
+const btnsOpenModal = document.querySelector(".show-modal");
+const signUP = document.querySelector(".btn btn-outline-primary");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+btnCloseModal2.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
