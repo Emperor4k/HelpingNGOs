ed9aefa8daf883c64df18102f010bb6a1c422877
Imperfect <86518966+Rksuryawanshi@users.noreply.github.com>
2023-03-29 00:05:22 +0530

    Update script.js
    kaan karna chahiye

--- script.js
+++ script.js
@@ -57,3 +57,23 @@ const getLocation = async (lat, long) => {
 btn.addEventListener("click", geo);
 
 // js for getMyLocation Ends here 
+
+
+
+// read more function
+
+function myFunction(i) {
+  var dots = document.getElementById(`dots${i}`);
+  var moreText = document.getElementById(`more${i}`);
+  var btnText = document.getElementById(`myBtn${i}`);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
